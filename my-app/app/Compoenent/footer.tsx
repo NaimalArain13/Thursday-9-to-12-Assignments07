8b6360239fe8c6ac165d6f65595bfbd6fbbef581
NaimalArain13 <naimalarain13@gmail.com>
2024-12-20 15:16:23 +0500

    class assignment 7 completed

---
+++ my-app/app/Compoenent/footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="p-3 bg-[#494226] flex items-center justify-center ">
+        <div className=" text-white px-2 md:px-7">
+           
+          <h1 className="  font-serif text-xl">
+            Assignment done by Student Leader: <span>Naimal Salahuddin</span>
+          </h1>
+        </div>
+       
+        
+    </div>
+  )
+}
+
+export default Footer
